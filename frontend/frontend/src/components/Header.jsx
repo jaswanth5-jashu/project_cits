911ba9fc6f62e6adf9c989be786239aa77dce3af
Pullareddy <pullareddyanakala7@gmail.com>
2026-01-26 10:34:11 +0530

    pipelinec & cpus adjusted

--- frontend/frontend/src/components/Header.jsx
+++ frontend/frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import '../css/Header.css';
 function Header() {
   return (
+    <div className="par">
     <header className="header">
       <div className="container">
         <div className="logo">
@@ -11,7 +12,14 @@ function Header() {
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About Us</NavLink>
           <NavLink to="/services">Services</NavLink>
-          <NavLink to="/pipeline">Pipeline</NavLink>
+          <div className="dropdown">
+            <span className="dropdown-title">Pipeline ▾</span>
+
+            <div className="dropdown-menu">
+              <NavLink to="/pipeline">Pipeline</NavLink>
+              <NavLink to="/cpus">CPUs</NavLink>
+            </div>
+          </div>
           <NavLink to="/mous">Mous</NavLink>
           <NavLink to="/gallery">Gallery</NavLink>
           <NavLink to="/give">Give Back</NavLink>
@@ -25,6 +33,7 @@ function Header() {
         </div>
         </div>
       </header>
+      </div>
   );
 }
 
